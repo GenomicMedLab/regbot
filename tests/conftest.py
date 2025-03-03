0c84b49d69c3a5e0cd8f08345ded3789382e23d0
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-03-03 11:11:49 -0500

    test: add noisy log suppression

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 """Provide basic test configuration and fixture root."""
 
+import logging
 from pathlib import Path
 
 import pytest
@@ -9,3 +10,22 @@ import pytest
 def fixtures_dir():
     """Provide path to fixtures directory."""
     return Path(__file__).resolve().parent / "fixtures"
+
+
+def pytest_addoption(parser):
+    """Add custom commands to pytest invocation.
+
+    See https://docs.pytest.org/en/8.1.x/reference/reference.html#parser
+    """
+    parser.addoption(
+        "--verbose-logs",
+        action="store_true",
+        default=False,
+        help="show noisy module logs",
+    )
+
+
+def pytest_configure(config):
+    """Configure pytest setup."""
+    if not config.getoption("--verbose-logs"):
+        logging.getLogger("requests_mock.adapter").setLevel(logging.INFO)
